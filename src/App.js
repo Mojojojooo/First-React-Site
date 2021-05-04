5a484d43075599c35e0f35d0344066d230c5bd42
Yashwanth Vernekar <yashvernekar674@gmail.com>
2021-05-04 17:23:14 +0530

    Input and OnChange Added input and Onchange event handler

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import { useState } from "react";
 // let color = "red";
 
 export default function App() {
-  const [count, setcount] = useState(0);
+  const [userInput, setUserInput] = useState("");
 
-  function clickHandle() {
-    console.log("Click Me! button is Clicked");
-    var countc = count + 1;
-    console.log(countc);
-    setcount(countc);
+  // function clickHandle() {
+  //   console.log("Click Me! button is Clicked");
+  //   var countc = count + 1;
+  //   console.log(countc);
+  //   setcount(countc);
+  // }
+
+  function inputChangeHandler() {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
   }
 
   return (
     <div className="App">
       <h1>Emoji Interpretor</h1>
-      <button onClick={clickHandle}>Click Me! {count}</button>
+      <input onChange={inputChangeHandler}></input>
+      <div> Welcome {userInput} </div>
+      {/* <button onClick={clickHandle}>Click Me! {count}</button> */}
     </div>
   );
 }
